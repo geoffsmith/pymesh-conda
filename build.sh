16f22addf6a68dd56dcfb7902e039d4666272185
Geoff Smith <geoff@geoff.me>
2019-06-14 10:29:20 +0100

    Working build

--- build.sh
+++ build.sh
@@ -1,21 +1,28 @@
+#!/bin/bash
+
+set -e
+
 export PYMESH_PATH=`pwd`
 
 git submodule update --init
 
+CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+
 cd $PYMESH_PATH/third_party
-mkdir build
+mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-make
+cmake -DCMAKE_BUILD_TYPE=Release ${CMAKE_PLATFORM_FLAGS[@]} ..
+make -j5
 make install
 
 cd $PYMESH_PATH
-mkdir build
+mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Release ${CMAKE_PLATFORM_FLAGS[@]} ..
 make
 # make tests
 
 cd $PYMESH_PATH
-./setup.py build
+# ./setup.py build  # I think this is the same as the above build stuff
 ./setup.py install
+
